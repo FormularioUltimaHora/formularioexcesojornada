a6a77c4b448836cbf27309fa8c28d2a57744e14e
datemaps2 <datemaps2@users.noreply.github.com>
2025-07-19 21:47:49 +0200

    feat: mensajes de consideraciones y advertencia añadidos a instrucciones del formulario

--- components/common/Instructions.tsx
+++ components/common/Instructions.tsx
@@ -8,6 +8,13 @@ export const Instructions: React.FC = () => {
       <p className="text-sm mb-4">
         Este formulario busca documentar de forma completa cada aspecto del servicio para tener una base sólida y detallada que permita demostrar, en caso de repetición, la intencionalidad del coordinador de asignar servicios que evidentemente resultarán en un exceso de jornada laboral, poniendo en riesgo la seguridad del trabajador y afectando negativamente su vida personal.
       </p>
+      <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4 rounded">
+        <strong>CONSIDERACIONES A TENER EN CUENTA:</strong><br/>
+        La hora de "la bajada" no es un derecho, la hora de la bajada es un gesto de cortesía por parte de la empresa para permitir el regreso de las unidades a la base de origen cumpliendo así con su hora de finalización de jornada, establecida actualmente 60 minutos antes de la finalización de la jornada. Esto no quiere decir que no se pueda asignar un servicio si faltan 60 minutos o menos para tu finalización de jornada. Por lo que, no se debe abusar del espacio de cortesía cedido por la empresa. Es tarea del coordinador asignar los servicios adecuados a cada unidad para cumplir así sus horarios.
+      </div>
+      <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4 rounded">
+        Los formularios completados incorrectamente, sin capturas de pantalla válidas o con señales de haber sido modificados intencionalmente serán descartados.
+      </div>
       <h3 className="text-md font-semibold text-slate-800">Instrucciones de Uso:</h3>
       <ol className="list-decimal list-inside space-y-2 mt-2 text-sm">
         <li>
