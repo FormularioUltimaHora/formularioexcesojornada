e03368603937df985f92dc39649c110d8737274d
datemaps2 <datemaps2@users.noreply.github.com>
2025-08-15 08:07:00 +0200

    FIX: Corregir ruta de importación de keepAlive para Vercel build

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { LoginPage } from './components/LoginPage';
 import { Instructions } from './components/common/Instructions';
 import { GeminiIcon } from './components/icons/GeminiIcon';
 import { ArchiveBoxIcon } from './components/icons/ArchiveBoxIcon';
-import { startKeepAliveService } from './utils/keepAlive';
+import { startKeepAliveService } from './src/utils/keepAlive';
 
 type FormVersion = 'A' | 'B';
 type View = 'forms' | 'login' | 'admin';
